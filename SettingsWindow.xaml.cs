2c4473792de4a7a31908af9cea9fe3f74ff57d95
Undefine-Unity <cheter153@gmail.com>
2020-08-16 17:29:30 +0200

    Settings Window Changed to work on a Grid

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -30,15 +30,13 @@ namespace SnakeWPF
 
         private void InitializeContent()
         {
-            //boardSize = 20;
-            
             // Main canvas
             Canvas cnvMain = new Canvas();
             cnvMain.Background = Brushes.Bisque;
             setMain.Content = cnvMain;
 
             //Settings Label
-            Label settingsLabel = new Label();
+            /*Label settingsLabel = new Label();
             settingsLabel.Content = "Settings";
             settingsLabel.FontSize = 52;
             Canvas.SetTop(settingsLabel, 0);
@@ -60,7 +58,43 @@ namespace SnakeWPF
             Canvas.SetTop(boardWidthInput, 80);
             Canvas.SetLeft(boardWidthInput, 100);
             //Canvas.SetRight(boardWidthInput, 140);
-            cnvMain.Children.Add(boardWidthInput);
+            cnvMain.Children.Add(boardWidthInput);*/
+
+            int rowsAmount = 5;
+            int colsAmount = 2;
+
+            Grid grdSet = new Grid();
+            Canvas.SetTop(grdSet, 0);
+            Canvas.SetLeft(grdSet, 0);
+            cnvMain.Children.Add(grdSet);
+
+            RowDefinition[] rows = new RowDefinition[rowsAmount];
+            for(int i = 0; i < rowsAmount; i++)
+            {
+                rows[i] = new RowDefinition();
+                rows[i].Height = new GridLength(25);
+                grdSet.RowDefinitions.Add(rows[i]);
+            }
+            
+            ColumnDefinition[] cols = new ColumnDefinition[colsAmount];
+            for(int i = 0; i < colsAmount; i++)
+            {
+                cols[i] = new ColumnDefinition();
+                cols[i].Width = new GridLength(200);
+                grdSet.ColumnDefinitions.Add(cols[i]);
+            }
+
+            Label lblBoardWidth = new Label();
+            lblBoardWidth.Content = "Board Width";
+            Grid.SetRow(lblBoardWidth, 0);
+            Grid.SetColumn(lblBoardWidth, 0);
+            grdSet.Children.Add(lblBoardWidth);
+
+            boardWidthInput = new TextBox();
+            boardWidthInput.TextChanged += boardWidthInput_TextChanged;
+            Grid.SetRow(boardWidthInput, 0);
+            Grid.SetColumn(boardWidthInput, 1);
+            grdSet.Children.Add(boardWidthInput);
         }
 
         private void boardWidthInput_TextChanged(object sender, RoutedEventArgs e)
